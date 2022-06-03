9a01a5acb64a87e08c2007b2775da25389bbb8f8
Ravi Jandyala <30474600+ravishanj@users.noreply.github.com>
2022-06-03 12:24:49 -0400

    create eks-kubernetes.tf

---
+++ eks-kubernetes.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
+    }
+  }
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "local"
+
+  config = {
+    path = "../terraform-aws-eks-workshop/src/terraform.tfstate"
+  }
+}
+
+# Retrieve EKS cluster information
+provider "aws" {
+  region = data.terraform_remote_state.eks.outputs.region
+#  access_key = var.aws_access_key
+#  secret_key = var.aws_secret_key
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_id
+}
+
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
